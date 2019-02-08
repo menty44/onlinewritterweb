1d2eb49e0a2f7d49df0ec434edd326f880f7b13c
menty44 <menty44@gmail.com>
2019-02-08 17:41:23 +0300

    done doing the login ui layout pending the login/register endpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {RegisterComponent} from './register/register.component';
 import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
