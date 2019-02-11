bf372b43c3707c124f505753502e1cbca3d824aa
menty44 <menty44@gmail.com>
2019-02-11 18:06:24 +0300

    added axios,lodash libs

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import axios from 'axios';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +12,29 @@ export class HeroesComponent implements OnInit {
 
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstormbbb'
   };
+  private fred: string;
+  private oluoch: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.fred = 'hello world';
+    // Make a request for a user with a given ID
+    axios.get('https://my-json-server.typicode.com/typicode/demo/posts')
+      .then(response => {
+        console.log(response.data);
+        _.forEach(response.data, (value, key) => {
+          console.log('lodash', value.title);
+        });
+        const mars = _.map(response.data, 'title');
+        console.log('lodahs map', mars);
+        this.oluoch = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 }
