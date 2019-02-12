4a080216f44d72b30e0ea0c88c0db8170725012e
Fredrick Oluoch <menty44@gmail.com>
2019-02-12 03:22:27 +0300

    nav bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'My First Angular App!';
+    public nav;
 
-    ngOnInit() {
+    constructor() { }
 
-        let auth = localStorage.getItem('profile'),
-            authparsed = JSON.parse(auth);
+    ngOnInit (){
+            let auth = localStorage.getItem('profile'),
+                authparsed = JSON.parse(auth);
 
-        if(authparsed !== null){
-            this.nav = authparsed.activated;
-        }else{
-            this.nav = '';
+            if(authparsed !== null){
+                this.nav = authparsed.activated;
+            }else{
+                this.nav = '';
+            }
         }
 
-
-    }
+    // ngOnInit() {
+    //
+    //     let auth = localStorage.getItem('profile'),
+    //         authparsed = JSON.parse(auth);
+    //
+    //     if(authparsed !== null){
+    //         this.nav = authparsed.activated;
+    //     }else{
+    //         this.nav = '';
+    //     }
+    // }
 }
